aacb34141dcbe5278fe95d62d699c532705ead8a
HCPTangHY <tanghy06025@163.com>
2024-01-01 20:26:57 +0800

    item icon func

--- game/01-PlayerFunc/NeargoPlayer.js
+++ game/01-PlayerFunc/NeargoPlayer.js
@@ -1,25 +1,61 @@
-var neargoPlayer = {
-    leftHand:"",
-    rightHand:"",
-    backpack:"",
-    inventory:[],
+// var neargoPlayer = {
+//     leftHand:window.neargoPlayer.leftHand || "",
+//     rightHand:window.neargoPlayer.rightHand || "",
+//     backpack:window.neargoPlayer.backpack || "",
+//     inventory:window.neargoPlayer.inventory || [],
 
-    move_to : function(target) {
-        V.outside = 0
-        V.location = target
-    },
-    travel : function(target) {
-        V.outside = 1
-        V.location = target
-    },
-    buy_item : function(args) {
+//     move_to : function(target) {
+//         V.outside = 0
+//         V.location = target
+//     },
+//     travel : function(target) {
+//         V.outside = 1
+//         V.location = target
+//     },
+//     buy_item : function(args) {
+//         let itemID=args[0];let price=args[1];
+//         console.log(this)
+//         if (window.neargoPlayer.inventory.length >=4) {return "空间不足"};
+//         window.neargoPlayer.inventory.push(itemID);
+//         V.money -= price*100;
+//         return "你购买了"+itemID
+//     }
+// }
+class NeargoPlayer {
+    constructor(lh,rh,bp,inv) {
+        this.leftHand = lh;
+        this.rightHand = rh;
+        this.backpack = bp;
+        this.inventory = inv;
+    }
+    init(lh,rh,bp,inv) {
+        this.leftHand = lh;
+        this.rightHand = rh;
+        this.backpack = bp;
+        this.inventory = inv;
+    }
+    update() {
+        Object.defineProperty(V,"neargoPlayer",{value:this});
+    }
+    move_to(target) {
+        V.outside = 0;
+        V.location = target;
+    }
+    travel(target) {
+        V.outside = 1;
+        V.location = target;
+    }
+    buy_item(args) {
         let itemID=args[0];let price=args[1];
-        console.log(window.neargoPlayer)
-        if (window.neargoPlayer.inventory.length >=4) {return "空间不足"};
-        window.neargoPlayer.inventory.push(itemID);
+        if (this.inventory.length >=4) {return "空间不足"};
+        if (V.money < price*100) {return "你没钱了！"}
+        this.inventory.push(itemID);
         V.money -= price*100;
-        return "你购买了"+itemID
+        this.update();
+        return "你购买了"+itemID;
     }
 }
-
-Object.defineProperty(window,"neargoPlayer",{value:neargoPlayer})
+if (window.neargoPlayer == undefined) {
+    let np = new NeargoPlayer("","","",[]);
+    Object.defineProperty(window,"neargoPlayer",{value:np});
+}
